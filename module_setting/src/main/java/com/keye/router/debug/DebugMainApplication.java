69ac618a7eac6d3e921cbe41cf50171078956b24
zhangkekekeke <liuyifei0zk@163.com>
2018-06-23 17:32:28 +0800

    feature:添加setting组件
    (cherry picked from commit c4737b6)

---
+++ module_setting/src/main/java/com/keye/router/debug/DebugMainApplication.java
@@ -0,0 +1,11 @@
+package com.keye.router.debug;
+
+
+import com.keye.router.lib_common.base.BaseApplication;
+
+/**
+ * Created by Administrator on 2018-06-21.
+ */
+
+public class DebugMainApplication extends BaseApplication {
+}
